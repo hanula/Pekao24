0458f68e8b2582086a0ec97043cb48b7f10d46bd
Sebastian Hanula <sebastian.hanula@gmail.com>
2014-09-20 13:54:39 +0200

    Updated parsing logic to work with current version of the website. Added requirements.txt.

--- pekao24/client.py
+++ pekao24/client.py
@@ -6,7 +6,7 @@ import cookielib
 import urlparse
 import urllib
 import getpass
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 
 
 class ConnectionError(Exception):
@@ -17,6 +17,9 @@ class AccessDenied(ConnectionError):
     MESSAGE = 'Wybrany serwis jest zablokowany'
 
 
+class ClientOutOfDate(ConnectionError):
+    MESSAGE = 'Klient jest nieaktualny'
+
 
 class Pekao24(object):
 
@@ -24,8 +27,8 @@ class Pekao24(object):
         self.client_id = client_id
         self.password = password
         self.connect()
-        self.read_accounts()
-        
+        self.accounts = {}
+
     def connect(self):
         br = mechanize.Browser()
 
@@ -55,13 +58,18 @@ class Pekao24(object):
 
         password_pattern = ''
 
-        soup = BeautifulSoup(response.get_data())
-        labels = soup.find(None, {'class':'accoutNumber'}).findAll('label')
-        for label in labels:
-            text = label.string.strip()
-            if text.startswith('#'):
+        # find password pattern
+        soup = BeautifulSoup(response.get_data(), from_encoding='utf8')
+        pattern = soup.find(class_='accoutNumber')
+        if not pattern:
+            raise ClientOutOfDate()
+
+        pattern = pattern.text.split(':')[1].replace(u'\xa0', '')
+
+        for part in pattern:
+            if part == '*':
                 password_pattern += '#'
-            elif text.startswith('['):
+            else:
                 password_pattern += '_'
 
         try:
@@ -69,48 +77,42 @@ class Pekao24(object):
         except mechanize.ControlNotFoundError:
             br.form.new_control('password', 'parPassword', {})
 
-        for idx, char in enumerate(self.password): 
+        for idx, char in enumerate(self.password):
             if password_pattern[idx] != '#':
                 br['parPassword'] += char
-        
+
         response = br.submit().get_data()
         if AccessDenied.MESSAGE in response:
             raise AccessDenied
-        
-        try:
-            br.follow_link(text_regex=re.compile('rachunki', re.IGNORECASE))
-            self.connected = True
-        except mechanize.LinkNotFoundError:
-            self.connected = False
-            raise ConnectionError
+
+        self.connected = True
         self.br = br
-        
+
     def _get_params(self, url):
         return urlparse.parse_qs(urlparse.urlparse(url).query)
-    
+
     def _get_opener(self):
         if not self.connected:
             raise ConnectionError
         return self.br
-        
+
     def open_link(self, text_regex):
         response = self._get_opener().follow_link(text_regex=re.compile(text_regex, re.IGNORECASE))
         soup = BeautifulSoup(response.get_data())
         self.content = soup.find(id='content')
         return self.content
-        
+
     def open_page(self, page_name, **params):
         current_params = self._get_params(self.br.geturl())
         current_params.update(params)
-        
+
         url = 'https://m.pekao24.pl/MCP/%s.htm?%s' % (page_name, urllib.urlencode(current_params, True))
         response = self._get_opener().open(url)
         soup = BeautifulSoup(response.get_data())
         self.content = soup.find(id='content')
         return self.content
-    
+
     def read_accounts(self):
-        self.open_link('rachunki')
         self.open_link('lista rachunk')
         self.accounts = {}
 
@@ -118,18 +120,20 @@ class Pekao24(object):
             cells = table.findAll('td')
             account_url = cells[0].find('a').get('href')
             account_id = urlparse.parse_qs(urlparse.urlparse(account_url).query).get('parAccNum')[0]
-            account_name = cells[0].text
+            account_name = cells[0].text.strip()
             val = cells[3].text.split()
             amount, currency = val[0], val[-1]
-            
+
             self.accounts[account_id] = {
                 'id': account_id,
                 'name': account_name,
                 'amount': amount,
                 'currency': currency
             }
-   
+
     def get_accounts(self):
+        if not self.accounts:
+            self.read_accounts()
         return self.accounts.values()
 
     def get_transaction_history(self, account_id):
@@ -140,31 +144,34 @@ class Pekao24(object):
         form = self.content.find('form', action=re.compile('CurrentAccount'))
         transactions = []
         form.find('select').extract()
-        
-        for div in form.findAll('div', recursive=False):
-            
+
+        for div in form.findAll('div', class_='box gray', recursive=False):
+
             if currency in div.text:
-                details_link = div.first('a', href=re.compile('parTransactionId'))
+
+                details_link = div.find('a', href=re.compile('parTransactionId'))
                 if details_link:
                     details_link.extract()
                     transaction_id = self._get_params(details_link.get('href')).get('parTransactionId')[0]
                     x = self._get_params(details_link.get('href')).get('parTransactionId')[0]
                 else:
                     transaction_id = ''
-                
-                contents = [el.string for el in div.firstText()]
-                
+
+                contents = div.text.strip().splitlines()
+
+                title = contents[1].strip().split(u'\xa0')[1].replace(currency, '')
+                amount = div.find('span').text
+
                 transaction = {
                     'id': transaction_id,
                     'date': contents[0].strip().strip(','),
                     'currency': currency,
-                    'amount': contents[1],
-                    'title': contents[-1],
+                    'amount': amount,
+                    'title': title,
                 }
                 transactions.append(transaction)
-                
-        return transactions                
-        
-        
+
+        return transactions
+
     def get_transaction_details(self, transaction_id):
         pass
